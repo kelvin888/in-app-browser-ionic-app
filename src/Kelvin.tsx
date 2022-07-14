b73ddf0333028adcbb5b5a73e20f8b77ce98a2f0
kelvin888 <kelvin.orhungul@gmail.com>
2022-07-14 11:47:29 +0100

    fetch from api before close

--- src/Kelvin.tsx
+++ src/Kelvin.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 export const Kelvin = () => {
+  useEffect(() => {
+    fetch("https://afrifex-backend.vercel.app/api/flights")
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        console.log({ data });
+        localStorage.setItem("data", JSON.stringify(data[0]));
+      });
+  }, []);
   return (
     <div className="App">
       <div id="message">
