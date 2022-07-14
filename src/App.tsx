28a5574db729340a39d8d47523dea623c8b8d23c
kelvin888 <kelvin.orhungul@gmail.com>
2022-07-14 10:16:55 +0100

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Aminat } from "./Aminat";
 import "./App.css";
+import { Kelvin } from "./Kelvin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Aminat />} />
-        <Route path="/kelv" element={<App />} />
+        <Route path="/kelv" element={<Kelvin />} />
       </Routes>
     </BrowserRouter>
   );
